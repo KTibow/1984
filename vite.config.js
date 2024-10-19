a2caa030358f3350673d6717ea00f9fbf896e4f0
KTibow <KTibow@users.noreply.github.com>
2024-10-18 17:45:47 -0700

    pt 5?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "",
   root: "src",
   publicDir: "../static",
   build: {
